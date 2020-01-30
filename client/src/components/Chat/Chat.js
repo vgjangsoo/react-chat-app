3facbedaebb7f97eff4b913c0a5d56d930cd4a15
Sungil Saul Lee <jangsoo86@gmail.com>
2020-01-29 18:18:27 -0800

    useEffect to save messages state

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,6 +7,8 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
 
     useEffect(() => {
@@ -28,6 +30,12 @@ const Chat = ({ location }) => {
         };
 
     }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        });
+    }, [messages]);
 
     return (
         <h1>Chat</h1>
