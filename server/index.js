8461a12c7bb8ec54f1107020e2e8a79b8a7fdbbc
Sungil Saul Lee <jangsoo86@gmail.com>
2020-01-29 18:11:01 -0800

    socket to broadcast that the user has joined the room

--- server/index.js
+++ server/index.js
@@ -18,9 +18,14 @@ io.on('connection', (socket) => {
 
         if(error) return callback(error);
 
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+
         socket.join(user.room);
+
+        callback();
     })
-    
+
     socket.on('disconnect', () => {
         console.log('User had left!!!');
     });
