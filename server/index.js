fa9583243d542ce7976af5ab78b36757a02d2bec
Sungil Saul Lee <jangsoo86@gmail.com>
2020-01-29 18:00:01 -0800

    server can know the users had joined in or not

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router')
@@ -11,12 +13,12 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!!!');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
 
-        
+        if(error) return callback(error);
+
+        socket.join(user.room);
     })
     
     socket.on('disconnect', () => {
