fbc6d400da741f33dbef6b72480a3ef480654a97
Sungil Saul Lee <jangsoo86@gmail.com>
2020-02-01 17:39:47 -0800

    minor code clean

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -7,15 +7,15 @@ import './InfoBar.css';
 
 const InfoBar = ({ room }) => {
     return (
-    <div className="infoBar">
-        <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon} alt="online image"/>
-            <h3>roomName</h3>
+        <div className="infoBar">
+            <div className="leftInnerContainer">
+                <img className="onlineIcon" src={onlineIcon} alt="online image"/>
+                <h3>roomName</h3>
+            </div>
+            <div className="rightInnerContainer">
+                <a href="/"><img src={closeIcon} alt="close image" /></a>
+            </div>
         </div>
-        <div className="rightInnerContainer">
-            <a href="/"><img src={closeIcon} alt="close image" /></a>
-        </div>
-    </div>
     )
 };
 
